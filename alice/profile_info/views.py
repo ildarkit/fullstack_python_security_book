761c7f152f5741bfba8f78bede17c6d96eec80e7
Ildar Shamiev <shamief@yandex.ru>
2023-11-18 18:17:56 +0300

    add tests for user authentication

--- alice/profile_info/views.py
+++ alice/profile_info/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 
 
-class ProfileView(View):
+class ProfileView(LoginRequiredMixin, View):
 
     def get(self, request):
-        user = request.user
-        if not user.is_authenticated:
-            return HttpResponse(status=401)
         return render(request, 'profile.html')
